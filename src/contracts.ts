07963c1c3b53aad8cbd537c6c101190eadcad251
Michael Oznobkin <michaeloznobkin@Michaels-MacBook-Pro-2.local>
2018-10-08 14:09:07 +0300

    fix: fte test

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,12 +1,12 @@
-import { ContractInfo } from './index';
+import { ContractInfo } from "./index";
 
 const payChannelContract: ContractInfo = {
-  address: '0x674f8b960d103ccbabc0b0201da0cd52b9f5d352',
-  abi: require('../jsonData/contractsABI/Dice.json').abi
+  address: "0xF4daC7a329BcAbC02C62d438D1f2dD226680B6F6",
+  abi: require("../jsonData/contractsABI/Dice.json").abi
 };
 
 const ERC20: ContractInfo = {
-  address: '0x5D1E47F703729fc87FdB9bA5C20fE4c1b7c7bf57',
-  abi: require('../jsonData/contractsABI/ERC20.json').abi
+  address: "0x5D1E47F703729fc87FdB9bA5C20fE4c1b7c7bf57",
+  abi: require("../jsonData/contractsABI/ERC20.json").abi
 };
 export { payChannelContract, ERC20 };
