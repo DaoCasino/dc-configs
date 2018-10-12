82bb9ff8f1b07c3b13def95042f96b886c2822b7
alexstep <alex.step.kellas@gmail.com>
2018-10-12 11:40:22 +0300

    Merge Alex and Harzu changes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
-import path from 'path';
-import { ERC20, Dice } from './contracts';
+import path from 'path'
+import { ERC20 } from './contracts'
 
 export interface ContractInfo {
-  address: string;
-  abi: any[];
+  address: string
+  abi: any[]
 }
 
 export interface IConfig {
-  privateKey: string;
-  web3HttpProviderUrl: string;
-  waitForConfirmations: number;
-  contracts: { ERC20: ContractInfo; Dice: ContractInfo };
-  gasPrice: number;
-  gasLimit: number;
-  DAppsPath: string;
-  signalServersSwarm: string[];
+  privateKey           : string
+  web3HttpProviderUrl  : string
+  waitForConfirmations : number
+  gasPrice             : number
+  gasLimit             : number
+  DAppsPath            : string
+  signalServersSwarm   : string[]
+
+  contracts:{ ERC20: ContractInfo }
 }
 
 // Infura Ropsten provide by default
-let rpcUrl: string = 'https://ropsten.infura.io/JCnK5ifEPH9qcQkX0Ahl';
+let rpcUrl: string = 'https://ropsten.infura.io/JCnK5ifEPH9qcQkX0Ahl'
 // in local(dev) env
-if (process.env.DC_NETWORK === 'local') rpcUrl = 'http://0.0.0.0:8545';
+if (process.env.DC_NETWORK === 'local') rpcUrl = 'http://0.0.0.0:8545'
 // provider set directly by env
 if (process.env.WEB_HTTP_PROVIDER_URL) {
-  rpcUrl = process.env.WEB_HTTP_PROVIDER_URL;
+  rpcUrl = process.env.WEB_HTTP_PROVIDER_URL
 }
 
 export const config: IConfig = {
@@ -31,7 +32,7 @@ export const config: IConfig = {
 
   web3HttpProviderUrl: rpcUrl,
   waitForConfirmations: 2,
-  contracts: { ERC20, Dice },
+  contracts: { ERC20 },
   gasPrice: Number(process.env.GAS_PRICE) || 40 * 1000000000,
   gasLimit: Number(process.env.GAS_LIMIT) || 40 * 100000,
 
@@ -44,4 +45,4 @@ export const config: IConfig = {
     '/dns4/signal2.dao.casino/tcp/443/wss/p2p-websocket-star/',
     '/dns4/signal3.dao.casino/tcp/443/wss/p2p-websocket-star/'
   ]
-};
+}
